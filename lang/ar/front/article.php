10c82f865462c2c7df79199138bc746a530c0ed5
Talha-SE <rtalha.online@gmail.com>
2025-08-03 01:07:23 +0500

    first commit

---
+++ lang/ar/front/article.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) Since 2024 InnoShop - All Rights Reserved
+ *
+ * @link       https://www.innoshop.com
+ * @author     InnoShop <team@innoshop.com>
+ * @license    https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+return [
+    'articles'            => 'صفحة المقالات',
+    'keyword'             => 'أدخل الكلمات الرئيسية',
+    'news_classification' => 'تصنيف الأخبار',
+    'news_tag'            => 'علامة الأخبار',
+    'search'              => 'بحث',
+];
